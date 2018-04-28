553ed121938da8f0a4d991b3c30d1a6a553d3b85
nnh <7bmzkr7h@sina.com>
2018-04-28 17:03:23 +0800

    1.Str中加split方法，调用后从Str转换为Arr 2.Arr的构造函数支持从之前的验证器数组中合拼

--- test/basic.js
+++ test/basic.js
@@ -2,21 +2,13 @@
 const { hephaestus, strategies: { Arr, Str, Num } } = require('../index');
 
 const schema = {
-    name: Str.of().non_empty()
-    , age: Num.of().condition(f).eq('17', 'eq error')
+    name: Str.of().non_empty().split(' ').fixed_length(2)
 };
 
 const aegis = hephaestus.forge(schema);
 
 const bank = aegis();
+bank.name = 'h w';
 
-bank.name = 'xd';
-bank.age = '17';
 
 console.log(bank);
-
-function f(x) {
-    if (this.name === 'xushen') {
-        if (x === '18') throw 'f error';
-    }
-}
