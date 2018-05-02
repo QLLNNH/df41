8184505cbde5e7419ee1b9c15cf925db8a516841
nnh <7bmzkr7h@sina.com>
2018-05-02 17:44:42 +0800

    1.添加Obj类

--- test/basic.js
+++ test/basic.js
@@ -1,13 +1,13 @@
 'use strict';
-const { hephaestus, Arr, Str, Num } = require('../index');
+const { hephaestus, Arr, Str, Num, Obj } = require('../index');
 
 const schema = {
-    name: Str.of().split(' ').limited_length(2).join('').eq('hw')
+    name: Obj.of()
 };
 
 const aegis = hephaestus.forge(schema);
 
 const bank = aegis();
-bank.name = 'h w';
+bank.name = {};
 
 console.log(bank);
