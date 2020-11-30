06139688ce2be9c7b4aeae6691d3f3953503216c
7bmzkr7h <7bmzkr7h@sina.com>
2020-11-30 16:55:54 +0800

    1.修复__catch方法有线抛出具体错误

--- tests/basic.js
+++ tests/basic.js
@@ -5,18 +5,10 @@ const { define, Arr, Boo, Num, Obj, Str, Time } = require('../index');
 
 try {
     const bank = define({
-        k: Time.of('PDATE invalid').extend((x) => {
-            if (x.getFullYear() < 2000) {
-                throw 'PDATE < 2000';
-            }
-            else if (x.getFullYear() > new Date().getFullYear()) {
-                throw 'PDATE too big';
-            }
-            return x;
-        })
+        k: Str.of('str err').gt(0, 'gt err')
     })();
 
-    bank.k = new Date();
+    bank.k = '';
     console.log(bank);
 }
 catch (err) {
