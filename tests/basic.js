ebfde93cca8c42ab66aabb6bd2500e5448af597c
7bmzkr7h <7bmzkr7h@sina.com>
2020-11-30 18:14:52 +0800

    1.修复extend函数中的this，使用动态绑定

--- tests/basic.js
+++ tests/basic.js
@@ -5,10 +5,10 @@ const { define, Arr, Boo, Num, Obj, Str, Time } = require('../index');
 
 try {
     const bank = define({
-        k: Str.of('str err').gt(0, 'gt err')
+        k: Str.of('str err').trim().eq(2, 'eq err')
     })();
 
-    bank.k = '';
+    bank.k = '222';
     console.log(bank);
 }
 catch (err) {
